677469e0627530a2c94c60960e4341b34f465dbf
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-12 22:31:49 +0100

    Add script installer

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+install() {
+    local SCRIPT_PATH="$(readlink -e "${BASH_SOURCE[0]}")"
+    local SCRIPT_NAME="$(basename "${SCRIPT_PATH}")"
+    local DIRECTORY_PATH="$(dirname "${SCRIPT_PATH}")"
+
+    local FILE_NAMES=(
+        .bash_profile
+        .bashrc
+    )
+
+    shopt -s nullglob
+
+    local FILE_NAME
+    for FILE_NAME in "${FILE_NAMES[@]}"; do
+        local FILE_DIRECTORY="${DIRECTORY_PATH}/${FILE_NAME}"
+        local DESTINATION="${HOME}/${FILE_NAME}"
+
+        if [ ! -f "${FILE_DIRECTORY}" ]; then
+            echo "File not found at ${FILE_DIRECTORY}"
+            continue;
+        fi
+
+        if [ -f "${DESTINATION}" ]; then
+            echo "File already exists at ${DESTINATION}"
+        else
+            ln -s "${FILE_DIRECTORY}" "${DESTINATION}"
+            echo "Installing ${FILE_NAME} to ${DESTINATION}"
+        fi
+    done
+
+    shopt -u nullglob
+}
+
+install
